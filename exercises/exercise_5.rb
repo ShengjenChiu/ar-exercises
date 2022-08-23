b57684bd6399266221edeb94e92c3fcf5d27f166
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-08-23 00:02:51 -0600

    one-to-many association

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+#1.
+# puts total_revenue = Store.annual_revenue
+
+# #2.
+# puts average_annual_revenue = 
+
+#3.
+# puts nunber_of_stores = Store.where("annual_revenue >": 1000000).count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Vicky", last_name: "Chan", hourly_rate: 60)
+@store2.employees.create(first_name: "Kevin", last_name: "Crosby", hourly_rate: 60)
+@store2.employees.create(first_name: "Jeremy", last_name: "Hubbard", hourly_rate: 60)
