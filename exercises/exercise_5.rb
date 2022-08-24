ec59b57dcb93543ea383fd3197c998a344fc7778
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-08-23 22:54:04 -0600

    exercise 5-2

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -9,15 +9,17 @@ puts "----------"
 
 # Your code goes here ...
 
-# 1. Output the total revenue for the entire company (all stores), using Active Record's `.sum` calculation method.
-# puts stores = Store.all
-# puts stores.annual_revenue.sum
-
+# 1. Output the total revenue for the entire company (all stores).
+@stores = Store.all
+puts all_annual_revenue = @stores.sum(:annual_revenue)
 
 # 2. On the next line, also output the average annual revenue for all stores.
-# puts average_annual_revenue = 
+store_count = Store.count
+puts average_annual_revenue = all_annual_revenue / store_count
 
 
+# 3. Output the number of stores that are generating $1M or more in annual sales. 
+# **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
+# @nunber_of_stores = Store.where("annual_revenue >=": 1000000).count
 
-# 3. Output the number of stores that are generating $1M or more in annual sales. **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
-# puts nunber_of_stores = Store.where("annual_revenue >": 1000000).count
+puts @nunber_of_stores
