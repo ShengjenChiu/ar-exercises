3db124412aec7355f141c1f32ae483e35ed085cc
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-08-25 15:20:35 -0600

    validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,34 +6,26 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
+require_relative '../lib/employee'
+require_relative '../lib/store'
+
+
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
-# 1. Add validations to two models to enforce the following business rules:
-# - Employees must always have a first name present
-validates :first_name, :string
+new_employee = Employee.new(store_id: 1, first_name: 'Hundson', last_name: 'Hornet', hourly_rate: 50)
+new_employee.save!
 
-# - Employees must always have a last name present
-validates :last_name, :string
-
-# - Employees have a hourly_rate that is a number (integer) between 40 and 200
-validates :hourly_rate, :integer { in: 40..200 }
-
-# - Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+puts new_employee.valid?
 
 
-# - Stores must always have a name that is a minimum of 3 characters
-
-
-# - Stores have an annual_revenue that is a number (integer) that must be 0 or more
-
-
-# 2. Ask the user for a store name (store it in a variable)
-
+# 2. Ask the user for a store name
+print "What is your store name: "
+@store_name = $stdin.gets.chomp
 
 # 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
-
-
+new_store = Store.new(name: @store_name)
+  
 # 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
-
+new_store.save!
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,16 @@
 class Employee < ActiveRecord::Base
+  # - Employees must always have a store that they belong to
   belongs_to :store
   
+  # 1. Add validations to two models to enforce the following business rules:
+  # - Employees must always have a first name present
+  validates :first_name, presence: true
+  
+  # - Employees must always have a last name present
+  validates :last_name, presence: true
+  
+  # - Employees have a hourly_rate that is a number (integer) between 40 and 200
+  validates :hourly_rate, numericality: { in: 40..200 }
+  
+
 end
